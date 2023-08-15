8e9c400f958e6b1f5df35e3ff4407a4be9e5469e
Thomas Bau <tbau11508@gmail.com>
2023-08-15 18:01:13 -0500

    Added logging to find path

--- v0.2.0/components/index.js
+++ v0.2.0/components/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { useLocation } from 'react-router-dom'
 
 import App from "./App";
 
+const location = useLocation()
+console.log(location.pathname);
+
 const appRouting = (
+  
   <Router>
     <Routes>
-      <Route path={`${window.location.pathname}`} element={<App/>} />
+      <Route path="/" element={<App/>} />
     </Routes>
   </Router>
 );
